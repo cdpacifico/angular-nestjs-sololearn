1c58f8c67d9e594ae355e8384df7dd9bba665272
cdpacifico <cripac@gmail.com>
2022-06-03 07:19:34 -0300

    Angular Class Vs Method Variables Intro

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,10 +3,12 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  styleUrls: ['./app.component.css'],
 })
+export class AppComponent {
+  // variables declared here will be class variables
 
-export class AppComponent  {
- 
-  name = "Star student";
+  speak() {
+    // variables declared here will be method variables
+  }
 }
